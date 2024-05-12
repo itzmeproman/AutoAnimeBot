3830ccdd30d7a852a3485d75271d2a26149ac486
itzmeproman <atulpandey3030@gmail.com>
2024-05-12 11:14:07 +0530

    Update info.py

--- functions/info.py
+++ functions/info.py
@@ -56,7 +56,7 @@ class AnimeInfo:
             return anime_name.strip()
 
     async def get_poster(self):
-        try:
+            try:
             if self.proper_name:
                 anime_poster = await self.kitsu.search(self.proper_name)
                 return anime_poster.get("poster_img") or None
